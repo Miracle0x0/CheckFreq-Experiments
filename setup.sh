4adab5c1f3e88a0ae215867d06adfe89fd697a4a
Junyi Zhang <Miracle_zjy@outlook.com>
2024-05-27 22:06:16 +0800

    Fix apex installation

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ pip3 install torch==1.8.2 torchvision==0.9.2 torchaudio==0.8.2 --extra-index-url
 
 # NVIDIA apex previous version
 git clone https://github.com/NVIDIA/apex
-# TODO: checkout to previous tag
-# TODO: disable CUDA version check
-pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --global-option="--cpp_ext" --global-option="--cuda_ext" ./
-
+# checkout to previous tag and disable CUDA version check
+cd apex && git checkout 22.04-dev && sed -i '179s/^/# /' setup.py && cd -
+cd apex && pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+cd -
